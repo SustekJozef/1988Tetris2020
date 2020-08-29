f5348433371a0a6feb069ac9caa5968f369a51cf
SustekJozef <sustekjozo@gmail.com>
2020-08-29 18:49:47 +0200

    Please enter the commit message for your changes. Lines starting

--- src/tetrisgame/Shape.java
+++ src/tetrisgame/Shape.java
@@ -3,24 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package tetrisgame;
 
 /**
  *
  * @author Jozef
  */
-public class TetrisGame {
+public class Shape {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-PlayBoard p=new PlayBoard();
-p.printWelcomeScreen();
-        
-        
-        
-        
-    }
-    
+
+
+
+
+
+
+
+
+
 }
